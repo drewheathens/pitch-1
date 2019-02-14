e673748ae2e6c5b7eb46701b6a4026c790be139e
drewheathens <mudavadie@gmail.com>
2019-02-14 13:17:43 +0300

    prod

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager, Server
 from flask_migrate import Migrate,MigrateCommand
 from app.models import User,Pitch,Comments
-app = create_app('development')
+app = create_app('production')
 
 
 manager = Manager(app)
@@ -22,6 +22,6 @@ def test():
 @manager.shell
 def make_shell_context():
     return dict(app = app, db = db, User = User, Pitch =Pitch,Comments=Comments )
-    
+
 if __name__ == '__main__':
     manager.run()
